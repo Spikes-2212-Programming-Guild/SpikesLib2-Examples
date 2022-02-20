2830931dd87aa6a26710c09bfaece4a01605c32c
OfriRosenbaum <ofriro1000@gmail.com>
2022-02-20 21:33:17 +0200

    created example climber

---
+++ src/main/java/frc/robot/subsystems/climber/Climber.java
@@ -0,0 +1,54 @@
+package frc.robot.subsystems.climber;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+
+import java.util.function.Supplier;
+
+public class Climber extends MotoredGenericSubsystem {
+
+    public static final double MAX_SPEED = 0.5;
+    public static final double MIN_SPEED = -0.5;
+
+    /**
+     * The speed in which the arms will move up to catch the bar (in order to climb).
+     *
+     * <p>An example game that this example subsystem can be used in is 2020 FRC INFINITE RECHARGE</p>
+     */
+    public final Supplier<Double> UP_SPEED = rootNamespace.addConstantDouble("up speed", 0.3);
+
+    /**
+     * The speed in which the arms will move down in order to take the robot up after they caught the bar.
+     */
+    public final Supplier<Double> DOWN_SPEED = rootNamespace.addConstantDouble("down speed", -0.4);
+
+    private static Climber instance;
+
+    public static Climber getInstance() {
+        if (instance == null) {
+            WPI_VictorSPX motor = new WPI_VictorSPX(RobotMap.CAN.CLIMBER_VICTOR);
+            motor.setNeutralMode(NeutralMode.Brake);
+            instance = new Climber(motor);
+        }
+        return instance;
+    }
+
+    private WPI_VictorSPX motor;
+
+    public Climber(WPI_VictorSPX motor) {
+        super(MIN_SPEED, MAX_SPEED, "climber", motor);
+        this.motor = motor;
+    }
+
+    /**
+     * Adds any commands or data from this subsystem to the {@link Shuffleboard}.
+     */
+    @Override
+    public void configureDashboard() {
+        rootNamespace.putData("move up", new MoveGenericSubsystem(this, UP_SPEED));
+        rootNamespace.putData("move down", new MoveGenericSubsystem(this, DOWN_SPEED));
+    }
+}
