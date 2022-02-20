a107c496284021bf86def1688dd619faf70e00c0
OfriRosenbaum <ofriro1000@gmail.com>
2022-02-20 21:02:14 +0200

    added other interfaces to RobotMap

--- src/main/java/frc/robot/subsystems/shooter/RobotMap.java
+++ src/main/java/frc/robot/subsystems/shooter/RobotMap.java
@@ -7,4 +7,16 @@ public class RobotMap {
         int SHOOTER_TALON_ONE = 1;
         int SHOOTER_TALON_TWO = 2;
     }
+
+    public interface DIO {
+
+    }
+
+    public interface PWM {
+
+    }
+
+    public interface AIN {
+
+    }
 }
