bffa3685d95cb360a2bcaef7754592d78ffa56c7
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-05 15:45:15 +0200

    added documentation

--- src/main/java/frc/robot/subsystems/gripper/Gripper.java
+++ src/main/java/frc/robot/subsystems/gripper/Gripper.java
@@ -4,14 +4,22 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
 import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import java.util.function.Supplier;
-
+/**
+ * A subsystem which represents a gripper. The gripper consists of two motors, that both rotate either inwards or
+ * outwards. If they both rotate inwards, the gripper intakes a cargo. If they both rotate outwards, the gripper
+ * dispenses the cargo.
+ */
 public class Gripper extends MotoredGenericSubsystem {
 
-    public final Supplier<Double> INTAKE_SPEED = rootNamespace.addConstantDouble("intake speed", 0.5);
-    public final Supplier<Double> DISPENSE_SPEED = rootNamespace.addConstantDouble("dispense speed", -0.5);
+    public static final double INTAKE_SPEED = 0.5;
+    public static final double DISPENSE_SPEED = -0.5;
 
+    /**
+     * With only a private constructor and a single private instance with a public getter, the singleton design pattern
+     * ensures that there is only one instance of a specific class, while providing public access to this instance.
+     */
     private static Gripper instance;
 
     public static Gripper getInstance() {
@@ -24,10 +32,13 @@ public class Gripper extends MotoredGenericSubsystem {
     }
 
     private Gripper(MotorController motorController1, MotorController motorController2) {
-        super("gripper", motorController1, motorController2);
+        super(DISPENSE_SPEED, INTAKE_SPEED, "gripper", motorController1, motorController2);
         motorController2.setInverted(true);
     }
 
+    /**
+     * Adds any commands or data from this subsystem to the {@link SmartDashboard}.
+     */
     @Override
     public void configureDashboard() {
         rootNamespace.putData("intake", new MoveGenericSubsystem(this, INTAKE_SPEED));
