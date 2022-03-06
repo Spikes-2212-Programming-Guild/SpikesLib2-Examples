047cc2eb5ab9bca434ee2badb394eb657db683e0
Yoel Perman Brilliant <yoyoperman@gmail.com>
2022-03-06 20:25:52 +0200

    added transfer

---
+++ src/main/java/frc/robot/subsystems/transfer/Robot.java
@@ -0,0 +1,77 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems.transfer;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import com.spikes2212.command.genericsubsystem.MotoredGenericSubsystem;
+import com.spikes2212.command.genericsubsystem.commands.MoveGenericSubsystem;
+import com.spikes2212.dashboard.Namespace;
+import com.spikes2212.dashboard.RootNamespace;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+
+    public static final double TRANSFER_SPEED = 0.5;
+
+    private RootNamespace robotNamespace;
+
+    @Override
+    public void robotInit() {
+        robotNamespace = new RootNamespace("robot");
+
+        MotoredGenericSubsystem transfer = new MotoredGenericSubsystem("transfer",
+                new WPI_VictorSPX(RobotMap.CAN.TRANSFER_VICTOR_1), new WPI_VictorSPX(RobotMap.CAN.TRANSFER_VICTOR_2));
+
+        robotNamespace.putData("move transfer", new MoveGenericSubsystem(transfer, TRANSFER_SPEED));
+    }
+
+    @Override
+    public void robotPeriodic() {
+        CommandScheduler.getInstance().run();
+    }
+
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+    }
+
+    @Override
+    public void autonomousInit() {
+
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopInit() {
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void simulationInit() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/transfer/RobotMap.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.transfer;
+
+public class RobotMap {
+
+    public interface CAN {
+
+        int TRANSFER_VICTOR_1 = 0;
+        int TRANSFER_VICTOR_2 = 1;
+    }
+    
+    public interface DIO {
+
+    }
+    
+    public interface PWM {
+
+    }
+    
+    public interface AIN {
+    
+    }
+}
