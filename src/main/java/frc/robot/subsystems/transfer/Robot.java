7f5b7ba8bb4bbebb591a86ec1ee930f7d4f372a5
OfriRosenbaum <ofriro1000@gmail.com>
2022-03-07 10:46:40 +0200

    fixed documentation

--- src/main/java/frc/robot/subsystems/transfer/Robot.java
+++ src/main/java/frc/robot/subsystems/transfer/Robot.java
@@ -21,15 +21,14 @@ public class Robot extends TimedRobot {
      */
     private final RootNamespace robotNamespace = new RootNamespace("robot");
 
-    /*
+    /**
      * A subsystem which transfers cargos. It consists of two MotorControllers that move a timing strap. The strap
-     * puts pressure on a ball, and thus the cargo moves along with the strap.
+     * puts pressure on a ball, and thus moves it along with the strap.
      */
     private MotoredGenericSubsystem transfer;
 
     @Override
     public void robotInit() {
-
         transfer = new MotoredGenericSubsystem("transfer",
                 new WPI_VictorSPX(RobotMap.CAN.TRANSFER_VICTOR_1), new WPI_VictorSPX(RobotMap.CAN.TRANSFER_VICTOR_2));
 
