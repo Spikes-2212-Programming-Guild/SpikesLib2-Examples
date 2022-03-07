e0fd50691981bb491cb7cd21d1aae6e4f71f8801
tal-sitton <tal.sitton04@gmail.com>
2022-03-07 10:17:24 +0200

    added the simple drivetrain

---
+++ src/main/java/frc/robot/drivetrains/simpleTankDrivetrain/RobotMap.java
@@ -0,0 +1,24 @@
+package frc.robot.drivetrains.simpleTankDrivetrain;
+
+public class RobotMap {
+
+    public interface CAN {
+
+        int LEFT_TALON_1 = 0;
+        int LEFT_TALON_2 = 1;
+        int RIGHT_TALON_1 = 2;
+        int RIGHT_TALON_2 = 3;
+    }
+
+    public interface DIO {
+        
+    }
+
+    public interface PWM {
+
+    }
+
+    public interface AIN {
+
+    }
+}
